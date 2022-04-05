0efa3f6d6ce1064e717bff14655fb41e37dce011
cristianarroyave <cristian.arroyave.clavijo@gmail.com>
2022-04-05 18:26:50 +0200

    Separar canvas y control para abrir primera puerta

--- Assets/Scripts/DoorAnimation.cs
+++ Assets/Scripts/DoorAnimation.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class DoorAnimation : MonoBehaviour
 {
 
+    public bool canBeOpened = false;
     [SerializeField] private Animator animator = null;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) {
+        if (other.CompareTag("Player") && canBeOpened) {
             Debug.Log("la puerta se abre");
             animator.Play("OpenDoor",0,0.0f);
             Debug.Log("deberia abrirse");
