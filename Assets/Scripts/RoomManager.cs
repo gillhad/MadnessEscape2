2eda007c72de750ac4956e67a241a1d926ab4e16
Gillhad <crbaronv@gmail.com>
2022-05-15 15:54:01 +0200

    cambios tamaño

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -43,16 +43,17 @@ public class RoomManager : MonoBehaviourPunCallbacks
         Vector3 spawnPosition1 = new Vector3(4f, 2f, 4f);
         Vector3 spawnPosition2 = new Vector3(8f, 2f, 25f);
         Vector3 spawnRoom3 = new Vector3(27f, 2f, 10f);
-        Vector3 spawnRoom2 = new Vector3(22f, 2f, 10f);
+        Vector3 spawnRoom2 = new Vector3(18f, 2f, 10f);
+        Vector3 spawnRoom22 = new Vector3(20f,2f,20f);
         Vector3 spawnRoom32 = new Vector3(27f,2f,25f);
         Vector3[] positions = new [] {spawnPosition1,spawnPosition2};
         
         if (PhotonNetwork.InRoom)
         {
             if(PhotonNetwork.IsMasterClient){
-                PhotonNetwork.Instantiate("FPSPlayer", spawnPosition1, Quaternion.identity);
+                PhotonNetwork.Instantiate("FPSPlayer", spawnRoom3, Quaternion.identity);
             }else{
-                PhotonNetwork.Instantiate("FPSPlayer", spawnPosition2, Quaternion.identity);
+                PhotonNetwork.Instantiate("FPSPlayer", spawnRoom32, Quaternion.identity);
             }
             
            
